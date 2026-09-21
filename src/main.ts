53cae5f0c35169b55720c57a9c2ba150d684fc31
Syed Yaseen Jamal <yaseenjamal.70.syj@gmail.com>
2026-09-21 13:16:42 +0500

    changed port to 3060

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3005);
+  await app.listen(process.env.PORT ?? 3060);
 }
 bootstrap();
